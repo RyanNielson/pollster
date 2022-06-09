c5bc4c842b0993baa7ce577f774c72b9ad6e8980
Ryan Nielson <ryan.nielson@gmail.com>
2022-06-09 11:40:33 -0300

    Custom page titles per page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,6 @@
 module ApplicationHelper
+  def page_title(*title_parts)
+    title = [title_parts, 'Pollster'].flatten.compact.join(' - ')
+    content_for(:title, flush: true) { title }
+  end
 end
